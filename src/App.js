686cbec3defb6fff5f9359473323f2ab0500b2e3
Aditya Dahake <100963388+dahakeadi15@users.noreply.github.com>
2023-04-07 16:37:34 +0530

    set up grid system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { Container, Grid } from "@mui/material";
+import SliderSelect from "./Components/SliderSelect";
+import TenureSelect from "./Components/TenureSelect";
+import Result from "./Components/Result";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Container maxWidth="xl" sx={{ marginTop: 4 }}>
+        <Grid container spacing={5} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <SliderSelect />
+            <TenureSelect />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Result />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
